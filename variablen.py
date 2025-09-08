60539aee93259bcf81d258cbe7ad86690ae45081
M-Florian <marc-florian@outlook.de>
2025-09-08 10:20:15 +0200

    changed variablen.py

--- variablen.py
+++ variablen.py
@@ -37,3 +37,17 @@ print(vorname, nachname)
 
 x2 =str(x)
 print("type(x):", type(x), "type(x2)", type(x2))
+
+# Bool
+wahre_aussage = True
+falsche_aussage = False
+print(type(wahre_aussage))
+
+# None
+none_database = None
+print(type(none_database))
+
+# ID
+
+print(id(x), id(y))
+print(id(x) - id(y))
