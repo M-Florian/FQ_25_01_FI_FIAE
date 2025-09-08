fcede12efde104cfb46e48df341669b0badb4bf0
M-Florian <marc-florian@outlook.de>
2025-09-08 09:40:49 +0200

    variablen.py create

---
+++ variablen.py
@@ -0,0 +1,39 @@
+# Variable vs Konstante
+## keine konstanten in Python!
+## ABER: Konvention - "Konstanten" werden IMMER groß geschrieben
+
+PI = 3.14
+MY_CONSTANT ="fsdfskdjf"
+
+
+# Numerisch
+#Dezimalsystem
+x = 10
+print("x:", x)
+
+y = 1_000_000_000
+print("y:", y)
+
+z = x + y
+
+print("Summe:", z)
+
+# Binär
+print("x(bin):", bin(x))
+
+# hex
+print("x(hex):", hex(x))
+
+# octal
+print("x(oct):", oct(x))
+
+# String:
+
+vorname = "Florian"
+nachname = "Nietmann"
+
+print(vorname + " " + nachname)
+print(vorname, nachname)
+
+x2 =str(x)
+print("type(x):", type(x), "type(x2)", type(x2))
